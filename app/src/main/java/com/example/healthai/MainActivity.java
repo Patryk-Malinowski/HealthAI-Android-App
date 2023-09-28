848113de9b8b0ad05d9ebcbc214e2ad274373ddc
Patryk Malinowski <145374860+Patryk-Malinowski@users.noreply.github.com>
2023-09-28 17:09:17 +0100

    Created a basic main/login screen with login and password fields. Created a functioning 'Forgot Your Password?' button that takes user to new activity upon pressing it.

---
+++ app/src/main/java/com/example/healthai/MainActivity.java
@@ -0,0 +1,46 @@
+// Patryk Malinowski
+// R00210173
+// HealthAI Android App
+
+
+package com.example.healthai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView txtForgotPassword = findViewById(R.id.textViewForgotPassword);
+
+        txtForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
