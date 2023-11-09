16ed19366ce9a6a1eb0c18e3bd2baab5613af0f2
Patryk Malinowski R00210173 <patryk.malinowski@mycit.ie>
2023-11-09 13:14:15 +0000

    Moved profile popup menu logic to ProfilePopupMenuHandler.java

--- app/src/main/java/com/example/healthai/MainActivity.java
+++ app/src/main/java/com/example/healthai/MainActivity.java
@@ -1,14 +1,14 @@
+// Patryk Malinowski
+// R00210173
+// HealthAI Android App
+
 package com.example.healthai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.PopupMenu;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,53 +25,13 @@ public class MainActivity extends AppCompatActivity {
             profileIcon.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    showPopupMenu(v);
+                    ProfilePopupMenuHandler.showPopupMenu(v, MainActivity.this);
                 }
         });
 
     }
 
-    private void showPopupMenu (View v){
-        PopupMenu popupMenu = new PopupMenu(this, v);
-        MenuInflater inflater = popupMenu.getMenuInflater();
-        inflater.inflate(R.menu.profile_menu, popupMenu.getMenu());
 
-        // set up click listener for profile menu items
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if (item.getItemId() == R.id.menu_my_details) {
-                    handleMyDetails();
-                    Log.d(TAG, "My Details clicked");
-                    return true;
-                } else if (item.getItemId() == R.id.menu_settings) {
-                    handleSettings();
-                    Log.d(TAG, "Settings clicked");
-                    return true;
-                } else if (item.getItemId() == R.id.menu_sign_out) {
-                    handleSignOut();
-                    Log.d(TAG, "Sign Out clicked");
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-        popupMenu.show();  // display pop up menu
-    }
-
-    // method for handling My Details click
-    public void handleMyDetails() {
-    }
-
-    // method for handling Settings click
-    public void handleSettings() {
-    }
-
-    // method for handling Sign Out click
-    public void handleSignOut() {
-    FirebaseAuthentication.SignOut(this);
-    }
 
 
     }
---
+++ app/src/main/java/com/example/healthai/ProfilePopupMenuHandler.java
@@ -0,0 +1,58 @@
+// Patryk Malinowski
+// R00210173
+// HealthAI Android App
+
+package com.example.healthai;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+
+public class ProfilePopupMenuHandler {
+    static String TAG = "ProfileMenuClick";
+
+    static void showPopupMenu (View v, Context context){
+        PopupMenu popupMenu = new PopupMenu(context, v);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.profile_menu, popupMenu.getMenu());
+
+        // set up click listener for profile menu items
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.menu_my_details) {
+                    Log.d(TAG, "My Details clicked");
+                    handleMyDetails(context);
+                    return true;
+                } else if (item.getItemId() == R.id.menu_settings) {
+                    Log.d(TAG, "Settings clicked");
+                    handleSettings(context);
+                    return true;
+                } else if (item.getItemId() == R.id.menu_sign_out) {
+                    Log.d(TAG, "Sign Out clicked");
+                    handleSignOut(context);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        popupMenu.show();  // display pop up menu
+    }
+
+    // method for handling My Details click
+    static void handleMyDetails(Context context) {
+    }
+
+    // method for handling Settings click
+    static void handleSettings(Context context) {
+    }
+
+    // method for handling Sign Out click
+    static void handleSignOut(Context context) {
+        FirebaseAuthentication.SignOut(context);
+    }
+}
