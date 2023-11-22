86141a374aa073e0f04523ea3a8c0353c53b011a
Patryk Malinowski R00210173 <patryk.malinowski@mycit.ie>
2023-11-22 16:13:15 +0000

    Fixed UserDetailsActivity displlaying all data instead of only the logged in user's.

--- app/src/main/java/com/example/healthai/UserDetailsActivity.java
+++ app/src/main/java/com/example/healthai/UserDetailsActivity.java
@@ -10,6 +10,9 @@ import android.util.Log;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -30,14 +33,20 @@ public class UserDetailsActivity extends AppCompatActivity {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        db.collection("users")
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String uid = currentUser.getUid();
+
+        db.collection("users").document(uid)
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {  // Change QueryDocumentSnapshot to DocumentSnapshot
                     @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         if (task.isSuccessful()) {
-                            StringBuilder userData = new StringBuilder();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                StringBuilder userData = new StringBuilder();
+
                                 insertData(userData, document, "name", "Full Name");
                                 insertData(userData, document, "gender", "Gender");
                                 insertData(userData, document, "dob", "Date of Birth");
@@ -51,10 +60,13 @@ public class UserDetailsActivity extends AppCompatActivity {
                                 insertData(userData, document, "seekbarValue5", "Seekbar Value 5");
                                 insertData(userData, document, "seekbarValue6", "Seekbar Value 6");
                                 insertData(userData, document, "seekbarValue7", "Seekbar Value 7");
+
+                                tv1.setText(userData.toString());
+                            } else {
+                                Log.d(TAG, "No such document");
                             }
-                            tv1.setText(userData.toString());
                         } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
+                            Log.w(TAG, "Error getting document.", task.getException());
                         }
                     }
                 });
@@ -65,7 +77,7 @@ public class UserDetailsActivity extends AppCompatActivity {
     }
 
 
-    private void insertData(StringBuilder userData, QueryDocumentSnapshot document, String field, String displayName) {
+    private void insertData(StringBuilder userData, DocumentSnapshot document, String field, String displayName) {
         // if document contains the specified field
         if (document.contains(field)) {
             // append custom display name, field value, and a newline character to the StringBuilder
